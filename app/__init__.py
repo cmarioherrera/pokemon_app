4d531bdba96ca7caecbf0232defabf87c1920f13
cmarioherrera <cmarioherrera@gmail.com>
2022-01-31 21:09:04 -0500

    Crear instancia principal y registrar app blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,10 @@ from flask import Flask
 
 def create_app(config_filename=None):
     app = Flask(__name__)
+    register_blueprints(app)
     return app
+
+
+def register_blueprints(app):
+    from app.pokemon.routes import pokemons_blueprint
+    app.register_blueprint(pokemons_blueprint)
