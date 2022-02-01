27936932414723ae145253734e103a3c4ecdaa23
cmarioherrera <cmarioherrera@gmail.com>
2022-01-31 21:06:35 -0500

    Agregar módulo de servicio

---
+++ app/pokemon/services.py
@@ -0,0 +1,39 @@
+from typing import List
+from dataclasses import dataclass
+
+from app.models import Pokemon
+
+
+class ListPokemonsQuery():
+
+    def execute(self) -> List[Pokemon]:
+        pokemons = Pokemon.get_pokemons()
+
+        return pokemons
+
+
+@dataclass
+class GetPokemonByNameQuery():
+    name: str
+
+    def execute(self) -> Pokemon:
+        pokemon = Pokemon.get_pokemon_by_name(self.name)
+
+        return pokemon
+
+
+@dataclass
+class GetPokemonByIdQuery():
+    id: int
+
+    def execute(self) -> Pokemon:
+        pokemon = Pokemon.get_pokemon_by_id(self.id)
+
+        return pokemon
+
+
+class OrderPokemonByAttackQuery():
+    def execute(self) -> List[Pokemon]:
+        pokemons = Pokemon.order_pokemon_by_attack()
+
+        return pokemons
