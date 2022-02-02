ef75f32e0f4a6f8f6f5706cef08a366065aef0d2
cmarioherrera <cmarioherrera@gmail.com>
2022-02-01 20:46:44 -0500

    Agregar respuesta & validar datos

--- app/pokemon/routes.py
+++ app/pokemon/routes.py
@@ -1,7 +1,8 @@
 
-from flask import jsonify, request
+from flask import request
 from . import pokemons_blueprint
 
+from app.pokemon.response import response, not_found
 from app.pokemon import services as pokemon_services
 
 
@@ -10,25 +11,26 @@ def pokemons():
     name = request.args.get('name', None)
     sort_by = request.args.get('sort_by', None)
     order_by = request.args.get('order_by', None)
+
     # filtered
     if name:
         query = pokemon_services.GetPokemonByNameQuery(name)
-        pokemons = query.execute().to_dict()
-    # sorted and order
+        pokemons = query.execute()
+        if pokemons is None:
+            return not_found()
+        return response(pokemons.to_dict())
+    # sorted
     elif sort_by and order_by:
-        if sort_by == 'attack':
-            query = pokemon_services.OrderPokemonByAttackQuery()
+        if sort_by == 'attack' and order_by == 'desc':
+            query = pokemon_services.OrderDescPokemonByAttackQuery()
             pokemons = [record.to_dict() for record in query.execute()]
+        elif sort_by == 'attack' and order_by == 'asc':
+            query = pokemon_services.OrderAscPokemonByAttackQuery()
+            pokemons = [record.to_dict() for record in query.execute()]
+        else:
+            return not_found()
     else:
         query = pokemon_services.ListPokemonsQuery()
         pokemons = [record.to_dict() for record in query.execute()]
 
-    return jsonify(pokemons)
-
-
-@pokemons_blueprint.route('/pokemons/<pokemon_id>/', methods=['GET'])
-def get_pokemon(pokemon_id: int):
-    query = pokemon_services.GetPokemonByIdQuery(id=pokemon_id)
-    pokemon = query.execute().to_dict()
-
-    return jsonify(pokemon)
+    return response(pokemons)
