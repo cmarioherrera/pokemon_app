a8de3c9ead4817a966fc3e636ca4d7b379e50c7c
cmarioherrera <cmarioherrera@gmail.com>
2022-02-02 23:24:00 -0500

    Agregar servicio de auth

---
+++ app/auth/services.py
@@ -0,0 +1,34 @@
+
+from dataclasses import dataclass
+
+from werkzeug.security import safe_str_cmp
+from app.models import User
+
+
+users = [
+    User(1, 'user1', 'password'),
+    User(2, 'user2', 'password')
+]
+
+
+@dataclass
+class AuthServices:
+
+    @classmethod
+    def get_usernames(cls):
+        return {user.username: user for user in users}
+
+    def get_user_ids(cls):
+        return {user.id: user for user in users}
+
+    @classmethod
+    def authenticate(cls, username, password):
+        users = cls.get_usernames()
+        user = users[username]
+        if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+            return user
+
+    @classmethod
+    def identity(cls, payload):
+        user_id = payload['identity']
+        return cls.get_user_ids.get(user_id, None)
