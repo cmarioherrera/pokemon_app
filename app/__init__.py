5277af42f4d12128d07fd58d27a01843713a1bd3
cmarioherrera <cmarioherrera@gmail.com>
2022-02-02 23:26:49 -0500

    Agregar jwt y  swagger (crear documentación para el API)

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,31 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
+
 from flasgger import Swagger
 
-swagger = Swagger()
+
+template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Title",
+        "description": "Pokemon API v1",
+        "version": "1.0.0"
+    },
+    "schemes": [
+        "http",
+        "https"
+    ],
+    "securityDefinitions": {
+        "Bearer": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header",
+        }
+    },
+}
+
+swagger = Swagger(template=template)
+jwt = JWTManager()
 
 
 def create_app(config_filename=None):
@@ -9,10 +33,11 @@ def create_app(config_filename=None):
 
     app.config['SWAGGER'] = {
         'title': 'Pokemon API v1',
-        'uiversion': 3
     }
+    app.config["JWT_SECRET_KEY"] = "super-secret"
 
     swagger.init_app(app)
+    jwt.init_app(app)
     register_blueprints(app)
     return app
 
@@ -20,3 +45,5 @@ def create_app(config_filename=None):
 def register_blueprints(app):
     from app.pokemon.routes import pokemons_blueprint
     app.register_blueprint(pokemons_blueprint, url_prefix='/api/v1')
+    from app.auth.routes import auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/api/v1')
